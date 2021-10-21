8eebfe649332e1d56a0ec7b00c4496bd94ecbef8
Gabriel Mihalache <mihalache.gabriel16@gmail.com>
2021-10-21 21:49:26 +0300

    tema

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Hello\nGabriel");
+
+        System.out.println(2 + 5);
+
+        float result = 10F / 3;
+        System.out.println(result);
+        // sau System.out.println(10F / 3);
+
+        System.out.println(-5 +8 * 6);
+        System.out.println((55+9) % 9);
+        System.out.println(20+ (-3)*5 / 8);
+        System.out.println(5+15 / 3 * 2 - 8 % 3);
+
+
+
+
     }
 }
